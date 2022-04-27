7764beea3ac44f7a035740bd027f3a944523430e
Asger Asgerov <asgaraa@code.edu.az>
2022-04-27 21:55:16 +0400

    final

---
+++ iticket/src/App.js
@@ -0,0 +1,14 @@
+
+import '../src/App.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import Home from './components/pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ iticket/src/components/Slider.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from './Header';
+
+function Slider() {
+    return (
+        <div className="container">
+            <Header/>
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src={require('../assets/img/slider/Slider1.jpeg')} alt="First slide" />
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src={require('../assets/img/slider/Slider2.jpeg')} alt="Second slide" />
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src={require('../assets/img/slider/Slider3.jpeg')} alt="Third slide" />
+                    </div>
+                </div>
+                <a className="carousel-control-prev lefticon" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    
+                </a>
+                <a className="carousel-control-next righticon" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Slider
